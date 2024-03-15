0c2e75ae52bc7eb53f0a2d58171b0bc1e76494c2
usmandeveloper007 <140097740+usmandeveloper007@users.noreply.github.com>
2024-03-15 15:54:50 +0500

    provider_statemanagement
    completing some small tasks with provider state management.

---
+++ lib/Light&DarkTheme/Screen/dark_theme_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+class DarkScreen extends StatefulWidget {
+  const DarkScreen({super.key});
+
+  @override
+  State<DarkScreen> createState() => _DarkScreenState();
+}
+
+class _DarkScreenState extends State<DarkScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: const Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
