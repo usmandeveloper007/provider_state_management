70836bff3a5069a9c6f91b3434046777dbcd230b
usmandeveloper007 <140097740+usmandeveloper007@users.noreply.github.com>
2024-03-15 16:49:04 +0500

    Changing light theme mode to dark theme mode

--- lib/Light&DarkTheme/Screen/dark_theme_screen.dart
+++ lib/Light&DarkTheme/Screen/dark_theme_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Provider/theme_changer_provider.dart';
 class DarkScreen extends StatefulWidget {
   const DarkScreen({super.key});
 
@@ -9,13 +12,36 @@ class DarkScreen extends StatefulWidget {
 class _DarkScreenState extends State<DarkScreen> {
   @override
   Widget build(BuildContext context) {
+    final themeChanger = Provider.of<ThemeChanger>(context);
     return Scaffold(
       appBar: AppBar(
-
+        title: Text('Theme Changer App'),
+        centerTitle: true,
+        // backgroundColor: Colors.greenAccent,
       ),
-      body: const Column(
+      body: Column(
         children: [
-
+          RadioListTile<ThemeMode>(
+            title: const Text('Light Mode'),
+              activeColor: Colors.greenAccent,
+              value: ThemeMode.light,
+              groupValue: themeChanger.themeMode,
+              onChanged: themeChanger.setTheme
+          ),
+          RadioListTile<ThemeMode>(
+              title: const Text('Dark Mode'),
+              activeColor: Colors.greenAccent,
+              value: ThemeMode.dark,
+              groupValue: themeChanger.themeMode,
+              onChanged: themeChanger.setTheme
+          ),
+          RadioListTile<ThemeMode>(
+              title: Text('System Mode'),
+              activeColor: Colors.greenAccent,
+              value: ThemeMode.system,
+              groupValue: themeChanger.themeMode,
+              onChanged: themeChanger.setTheme
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:providers/Light&DarkTheme/Provider/theme_changer_provider.dart';
 import 'package:providers/Light&DarkTheme/Screen/dark_theme_screen.dart';
 
 void main() {
@@ -11,13 +13,41 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const DarkScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeChanger()),
+      ],
+      child: Builder(
+          builder: (BuildContext context){
+            final themeChanger = Provider.of<ThemeChanger>(context);
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              themeMode: themeChanger.themeMode,
+              theme: ThemeData(
+                brightness: Brightness.light,
+                // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                primarySwatch: Colors.red,
+                appBarTheme: AppBarTheme(
+                  color: Colors.red
+                )
+                // useMaterial3: true,
+              ),
+              darkTheme: ThemeData(
+                brightness: Brightness.dark,
+                primarySwatch: Colors.yellow,
+                primaryColor: Colors.greenAccent,
+                appBarTheme: AppBarTheme(
+                  color: Colors.greenAccent
+                ),
+                iconTheme: IconThemeData(
+                  color: Colors.greenAccent
+                )
+              ),
+              home: const DarkScreen(),
+            );
+          }
+      )
     );
   }
 }
